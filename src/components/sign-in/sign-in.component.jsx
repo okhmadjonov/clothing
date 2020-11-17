676d50b23a9231f3cb048bfaf7dde960d3d0cd0e
okhmadjonov <oakhmadjonov1@gmail.com>
2020-11-17 10:54:53 +0500

    integreted firebase and sign in by google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
 
-import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor() {
@@ -39,7 +39,7 @@ class SignIn extends React.Component {
             label="email"
             required
           />
-         
+
           <FormInput
             name="password"
             type="password"
@@ -48,13 +48,12 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          
-
+         <div className='buttons'>
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}
-          isGoogleSignIn>
-         
-          Sign in with Google </CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign in with Google{" "}
+          </CustomButton>
+          </div>
         </form>
       </div>
     );
